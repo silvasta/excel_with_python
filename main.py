265b68d0ed71bd271d2ecc675c047821e76be119
Silvan Stadelmann <silvasta3@gmail.com>
2022-11-26 01:54:18 +0100

    import complete

--- main.py
+++ main.py
@@ -20,57 +20,48 @@ def display_all(data):
         print("Geschoss: {}".format(geschoss["name"]))
         for aktion in geschoss["aktion"]:
             print("----------------------------------------------")
-            print("Aktion: {}".format(aktion["name"]))
+            print("\nAktion: {}\n".format(aktion["name"]))
             for messwert in aktion["messwert"]:
-                print(messwert["name"])
+                print("Messwert: {}".format(messwert["name"]))
+                #print("aufschalten: {}".format(messwert["aufschalten"]))
         print("----------------------------------------------")
 
-# Importiert das Dokument und gibt dieses sowie das aktuelle Blatt zurück
+# Importiert das Dokument und gibt dieses sowie das aktuelle Arbeitsblatt zurück
 def import_file(path):
     wb = load_workbook(filename = path) 
     ws = wb.active
     return wb, ws 
 
-# Wandelt das Blatt in eine Python Datenstruktur bringt
+# Wandelt das Arbeitsblatt in eine Python Datenstruktur bringt
+# data: Liste mit Geschossen
+# geschoss: Dictionary mit Atributen und Liste von Aktionen
+# aktion: Dictionary mit Atributen und Liste von Messwerten
+# messwert: Dictionary mit Atributen
 def get_data(sheet):
-    i_geschoss, i_aktion = 1, 0
     data = []
     for row in sheet.iter_rows(min_col = 5, max_col = 5, max_row = 2000):
         for cell in row:
             if cell.value == None:
                 continue
             split = str(cell.value).split("/")
-            if split[0] == str(i_geschoss) and split[1] == "-":
+            if split[1] == "-" and split[2] == "-":
                 name = sheet.cell(row[0].row, column = 1).value
                 geschoss = {"name": name, 
-                            "id_geschoss": i_geschoss, 
+                            "id_geschoss": int(split[0]), 
                             "aktion": []}
                 data.append(geschoss)
-                i_geschoss += 1
-                i_aktion = 0
-            elif split[1] >= str(i_aktion) and split[2] == "-":
-                i_aktion = int(split[1])
+            elif split[2] == "-":
                 name = sheet.cell(row[0].row, column = 2).value
-                if sheet.cell(row[0].row, column=6).value == "true":
-                    bool_f = True 
-                else:
-                    bool_f = False
+                bool_f = sheet.cell(row[0].row, column=6).value == "true"
                 aktion = {"name": name, 
-                          "id_aktion": i_aktion, 
+                          "id_aktion": int(split[1]), 
                           "bool_f": bool_f,
                           "messwert": []}
                 data[-1]["aktion"].append(aktion)
-                i_aktion += 1
             else:
                 name = sheet.cell(row[0].row, column = 3).value
-                if sheet.cell(row[0].row, column=6).value == "true":
-                    aufschalten = True 
-                else:
-                    aufschalten = False
-                if sheet.cell(row[0].row, column=6).value == "true":
-                    bool_f = True 
-                else:
-                    bool_f = False
+                aufschalten = sheet.cell(row[0].row, column=4).value == "x"
+                bool_f = sheet.cell(row[0].row, column=6).value == "true"
                 kommentar = sheet.cell(row[0].row, column = 7).value
                 dpst = sheet.cell(row[0].row, column = 8).value
                 messwert = {"name": name,
@@ -79,7 +70,6 @@ def get_data(sheet):
                             "bool_f": bool_f,
                             "kommentar": kommentar,
                             "dpst": dpst, }               
-                #data[i_geschoss-2]["aktion"]["messwert"]
                 data[-1]["aktion"][-1]["messwert"].append(messwert)
     return data
 
